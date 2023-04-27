039042c50dd55df2df5cc33516ccb492cbf98718
Caleb Martin <cmartin@moderncaliber.com>
2023-04-27 09:11:38 -0400

    removed call to Products module scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack5: true,
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config
+  },
 }
 
 module.exports = nextConfig

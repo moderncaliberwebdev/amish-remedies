44964bc9dc9dd25cd146c566d3298f64f320d3ed
Caleb Martin <cmartin@moderncaliber.com>
2023-05-11 09:43:50 -0400

    categories mobile & terms and conditions

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,262 @@
+import React from 'react'
+import Layout from '../Components/Layout'
+import styles from '../styles/Terms.module.scss'
+
+export default function Terms() {
+  return (
+    <Layout>
+      <div className={styles.terms}>
+        <h1>Terms and Conditions </h1>
+        <h2>Privacy Policy</h2>
+        <p>
+          We are fully committed to protecting your privacy. When you place an
+          order with us, none of your payment information is saved on our site.
+          All your card information etc is safe and secure as it is processed
+          thought Paypal and we never have access to it. If your using a credit
+          card to pay for your order they may be limits on how much your card
+          can be used towards your purchase. This is for your protection and
+          ours. After your order is placed we are then told where to ship your
+          order though the payment processor. When purchasing with a credit card
+          your card statement will show payment was made to DeStefano
+          Enterprises, LLC. We have put security measures in place to protect
+          you against unauthorized access, loss, misuse, and alteration of your
+          personal information that we collect. Old Amish Remedies Plus
+          (DeStefano Enterprises, LLC) reserves the right to deny any credit
+          card purchase at anytime with no explanation required. If a credit
+          card sale seems at risk of a charge back for any reason whatsoever,
+          Old Amish Remedies Plus, LLC. reserves the right to deny any credit
+          card by any customer without implications of any kind. Old Amish
+          Remedies Plus, LLC. Is required to perform a Compliance test on the
+          system every 3 months. Any Credit card and payment info is not stored
+          in our database. And our web site is required to pass strict
+          compliance tests. There is a "email this link to a friend" feature on
+          our site that allow you to easily share product or other valuable
+          information with your friends. Any e-mail addresses provided to us in
+          the use of these features are used solely to deliver that particular
+          e-mail. They are NOT retained in our database. If we change this
+          Privacy Policy in whole or in part, we will inform you by posting a
+          notice on this site. Any changes will go into effect on the date they
+          are posted and will apply to all users. If you have any questions or
+          comments about our Privacy Policy, feel free to contact us. Welcome to
+          Old Amish Remedies Plus (DeStefano Enterprises, LLC). Your use of this
+          site demonstrates your acceptance of the following terms & conditions.
+          If you do not agree to these terms & conditions in whole or in part,
+          please do not use this site.
+        </p>
+        <h2>Privacy</h2>
+        <p>
+          Please review our Privacy Policy to understand our practices regarding
+          privacy. Electronic Communications - You agree to receive
+          communications from us electronically (via e-mail). You agree that all
+          agreements, notices, disclosures, and other communications we provide
+          to you electronically satisfy any and all legal requirements that such
+          communications be in writing.
+        </p>
+        <h2>Product Reviews</h2>
+        <p>
+          Unless indicated otherwise, when you post content or submit material
+          on our site, you grant Old Amish Remedies Plus (DeStefano Enterprises,
+          LLC.) the right to use, reproduce, modify, adapt, publish, translate,
+          create derivative works from, distribute, and display such content
+          throughout the world in any media. You also grant Old Amish Remedies
+          Plus and its sub-licensees the right, but not the obligation, to use
+          the name that you submit with such content. You represent and warrant
+          that you own or control all rights to the content you post or submit;
+          that the content is accurate; that use of the content will not violate
+          this policy; and that the content will not cause injury to any person
+          or entity. You agree to indemnify Old Amish Remedies Plus for any and
+          all claims resulting from content that you submit or post. You agree
+          that Old Amish Remedies Plus has the right, but not the obligation, to
+          monitor, edit, and/or remove any activity or content. Old Amish
+          Remedies Plus (DeStefano Enterprises LLC.) shall have no
+          responsibility and assumes no liability for any content posted by you
+          or anyone else.
+        </p>
+        <h2>Copyright & Trademarks</h2>
+        <p>
+          This website and its contents are intended for your personal use.
+          Unless otherwise noted, all content on this site, including text,
+          graphics, logos, button icons, images, audio clips, and software, is
+          the property of Old Amish Remedies Plus (DeStefano Enterprises LLC.)
+          and is protected by United States and international copyright laws.
+          All software used on this site is the property of Old Amish Remedies
+          Plus and is protected by United States and international copyright
+          laws. Third-party content providers also own copyrights in the content
+          original to it. You may not modify, publish, adapt, transmit,
+          participate in the transfer or sale, exhibit, distribute, display,
+          reverse engineer, decompile, dissemble, perform, reproduce, create
+          derivative works from, or in any way exploit any of the site content,
+          on this site, in whole or in part. You may download or copy one copy
+          of the site content for personal, noncommercial use only, provided
+          that no changes in or deletion of author attribution, trademark,
+          legend, copyright notice, or other proprietary notices shall be made.
+          Downloading or copying site content for other than personal,
+          noncommercial use is expressly prohibited without the prior written
+          permission of us or any other copyright owner. You acknowledge that
+          you do not acquire any ownership rights by downloading or copying any
+          of the product listings, descriptions, or prices, nor may you frame or
+          utilize framing techniques to enclose any trademark, logo, or
+          copyrighted material from the site or use any meta tags or any other
+          hidden text utilizing our names or trademarks, without our express
+          written consent. You shall be solely responsible and liable for any
+          and all damages resulting from any infringement of copyrights,
+          proprietary rights, or any other harm resulting from any uploading,
+          posting, or submission of your content on the Site. 2023 Old Amish
+          Remedies Plus. All rights not expressly granted herein are reserved.
+        </p>
+        <h2>Product Descriptions and Prices</h2>
+        <p>
+          We attempt to describe products featured on this site as accurately as
+          possible. However, we do not warrant that product descriptions or
+          other content of this site is accurate, complete, reliable, current,
+          or error-free. In the event a product is listed at an incorrect price
+          or with incorrect information due to a typographical error or an error
+          in pricing or product information received from our suppliers, we
+          shall have the right to refuse or cancel any orders placed for
+          products listed at the incorrect price. We shall have the right to
+          refuse or cancel any such orders whether or not the order has been
+          confirmed and your credit card charged. If your credit card has
+          already been charged for the purchase and your order is canceled, we
+          shall immediately issue a credit to your credit card account in the
+          amount of the charge. We apologize for any inconvenience this may
+          cause you. (If you are not completely satisfied with your purchase,
+          you may return it as described in our Return Policy)
+        </p>
+        <h2>Copyright Infringement Claims</h2>
+        <p>
+          If you believe that any material or content posted on this site
+          constitutes copyright infringement, please contact us and we will make
+          all efforts to remedy the situation as quickly as possible. (This
+          notice is issued pursuant to the Online Copyright Infringement
+          Liability Limitation Act.)
+        </p>
+        <h2>Protecting Children</h2>
+        <p>
+          We are careful to protect the safety and privacy of children. Our web
+          sites are general audience sites. Children under the age of 18 should
+          always ask their parents or guardians for permission before providing
+          any personal information to anyone online. We urge parents and
+          guardians to participate in their children's online activities and use
+          parental control or other web filtering technology to supervise
+          children's access to the Internet.
+        </p>
+        <h2>Links to Third-Party Sites</h2>
+        <p>
+          This site may contain links to web sites operated and maintained by
+          third parties, over which we have no control. Their Privacy Policy and
+          Terms & Conditions may be different from our Privacy Policy and Terms
+          & Conditions. Be advised that you access such linked sites at your own
+          risk. You should always read the privacy policy of a linked site
+          before disclosing any personal information on it.
+        </p>
+        <h2>Disclaimer of Warranties</h2>
+        <p>
+          EXCEPT AS EXPRESSLY STATED HEREIN, WE MAKE NO REPRESENTATIONS OR
+          WARRANTIES, EITHER EXPRESS OR IMPLIED, OF ANY KIND WITH RESPECT TO
+          PRODUCTS SOLD ON OUR SITE. EXCEPT AS EXPRESSLY STATED HEREIN, WE
+          EXPRESSLY DISCLAIM ALL WARRANTIES, EXPRESS OR IMPLIED, OF ANY KIND
+          WITH RESPECT TO PRODUCTS SOLD ON OUR SITE, INCLUDING BUT NOT LIMITED
+          TO, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. BY USING AND
+          PURCHASING PRODUCTS FROM THIS SITE, YOU AGREE THAT IN NO EVENT SHALL
+          Old Amish Remedies Plus (DeStefano Enterprises, LLC,) ITS DIRECTORS,
+          OFFICERS, EMPLOYEES OR OTHER REPRESENTATIVES BE LIABLE FOR SPECIAL,
+          INDIRECT, CONSEQUENTIAL, OR PUNITIVE DAMAGES RELATED TO PRODUCTS SOLD.
+        </p>
+        <h2>Applicable Law</h2>
+        <p>
+          You agree to comply with all applicable laws, statutes, ordinances,
+          and regulations regarding your use of our Site and your purchase of
+          products or services through our Site. We may, in our sole discretion,
+          report actual or perceived violations of law to law enforcement or
+          appropriate authorities. If we become aware of any potential or
+          suspected violation of these Terms & Conditions or of our Privacy
+          Policy we may (but are not obligated to) conduct an investigation to
+          determine the nature and extent of the suspected violation and the
+          appropriate enforcement action, during which investigation we may
+          suspend services to any customer being investigated and/or remove any
+          material from our servers. You agree to cooperate fully with any such
+          investigation. You acknowledge that violations of the Terms &
+          Conditions or the Privacy Policy could be subject to criminal or civil
+          penalties. The Terms & Conditions and the relationship between you and
+          Old Amish Remedies Plus (DeStefano Enterprises, LLC.) shall be
+          governed by the laws of the State posted in the address below without
+          regard to its conflict of law provisions. The failure of Old Amish
+          Remedies Plus (DeStefano Enterprises, LLC.) to exercise or enforce any
+          right or provision of the Terms & Conditions shall not constitute a
+          waiver of such right or provision. If any provision of the Terms &
+          Conditions is found by a court of competent jurisdiction to be
+          invalid, the parties nevertheless agree that the court should endeavor
+          to give effect to the parties' intentions as reflected in the
+          provision, and the other provisions of the Terms & Conditions remain
+          in full force and effect. You agree that regardless of any statute or
+          law to the contrary, any claim or cause of action arising out of or
+          related to use of this site or the Terms & Conditions must be filed
+          within one (1) year after such claim or cause of action arose or be
+          forever barred.
+        </p>
+        <h2>Contact us here:</h2>
+        <p>
+          Old Amish Remedies Plus P.O. Box 264, Bowmansville, Pennsylvania 17507
+          Or call: (717) 826-3754
+        </p>
+        <h2>Shipping Policy</h2>
+        <p>
+          Please check with us via email or phone for shipping lead times. The
+          item is estimated to ship from the "Shop" in the time frame given once
+          payment has been received and cleared. You will receive the item after
+          that date based on delivery time of the carrier. Typically a product
+          will be delivered within 2-3 business days upon picked up at our
+          "Shop". Occasionally, due to unforeseen circumstances, an order may
+          take longer to leave. If this happens we will be sure to contact you
+          and let you know when your item will ship. Although your item may ship
+          out immediately it may take longer due to any unforeseen
+          circumstances. We reserve the right to use any carrier.
+        </p>
+        <h2>Return Policy</h2>
+        <p>
+          Your satisfaction is our top priority. We sell only the finest quality
+          products, so we rarely have problems with any of our products. But if
+          your item is damaged or defective in some way, we'll make sure you're
+          taken care of. If your item has a defect or was damaged during
+          shipping, contact us and we'll help you get the replacement product
+          you need or arrange for total replacement of the product at no cost to
+          you. Please find your specific situation from the list of scenarios
+          below to learn how we handle that particular situation:
+          <br />
+          <br />
+          1. You decided to cancel your order: If we have started processing of
+          your order but it has not shipped, we can cancel the order and issue
+          you a full refund.
+          <br />
+          <br />
+          2. You received the product but the product is not as described. If
+          the item is damaged or defective but doesn't appear to be from the
+          shipping of the product, we will work reasonably to correct the issue
+          with you. If the issue cannot be resolved we reserve the right to
+          cancel the order and recover the product. Please contact our office
+          immediately after you receive your product if you notice any issues at
+          all with the product quality.
+          <br />
+          <br />
+          3. You received an item that appears to have been damaged during
+          shipping: Please be sure to inspect all packages upon arrival and
+          refuse the package if it appears to be damaged during shipping unless
+          a resolution between Old Amish Remedies Plus the carrier and yourself
+          has been made. Have the carrier return to sender and contact our
+          office. If a package appears to have been damaged during shipping, the
+          item is oftentimes still okay. If the packaging for your item appears
+          to be damaged, be sure to keep all the original packaging until you've
+          determined whether or not a shipping insurance claim needs to be
+          submitted.
+          <br />
+          <br />
+          4. You've received the product but you've decided you want to return
+          it: We cannot accept returns on personalized items, special order
+          items, custom items, or items damaged through normal wear and tear.
+          Large returned orders though no fault of our doing is subject to a 15%
+          restocking fee plus all shipping costs will be paid by customer.
+        </p>
+      </div>
+    </Layout>
+  )
+}
